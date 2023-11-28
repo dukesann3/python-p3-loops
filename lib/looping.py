2f51da5e88c1c99998355bcb23dd43c1f738bbc9
Duke <dukemorise33@gmail.com>
2023-11-28 15:05:24 -0800

    completed lesson

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
+import ipdb
 
 def happy_new_year():
-    # code goes here!
-    pass
+
+    reversed_range = list(reversed(range(1,11)))
+
+    for i in reversed_range:
+        print(f"{i}")
+    
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+
+    squared_list = []
+    for int_in_list in int_list:
+        squared_list.append(int_in_list**2)
+
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+
+    for count in range(1,101):
+        if count % 5 == 0 and count % 3 == 0:
+            print("FizzBuzz")
+        elif count % 3 == 0:
+            print("Fizz")
+        elif count % 5 == 0:
+            print("Buzz")
+        else:
+            print(f"{count}")
+
+
